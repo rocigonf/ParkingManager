f60ab55356840fc907f87966b60f5b3eacd986eb
JorgeReina <reinaromerojorge@gmail.com>
2023-01-18 20:40:22 +0100

    Modificación archivos "User" y "Controller"

--- parkingmanagerDemo/src/main/java/com/rociojorge/parkingmanagerDemo/user/User.java
+++ parkingmanagerDemo/src/main/java/com/rociojorge/parkingmanagerDemo/user/User.java
@@ -1,4 +1,29 @@
 package com.rociojorge.parkingmanagerDemo.user;
 public class User {
     
+    private long id;
+    private String email;
+    private String nombre;
+
+    public User(String email, String nombre) {
+        this.email = email;
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
 }
--- parkingmanagerDemo/src/main/java/com/rociojorge/parkingmanagerDemo/user/UserController.java
+++ parkingmanagerDemo/src/main/java/com/rociojorge/parkingmanagerDemo/user/UserController.java
@@ -1,5 +1,14 @@
 package com.rociojorge.parkingmanagerDemo.user;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
     
+
+    private UserRepository repository = new UserRepository();
+        
+    public String displayUsers() {
+        
+    }
 }
