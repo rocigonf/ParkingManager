0135ef0a4c0591e9c9f43d20f40b9674132dd1f1
JorgeReina <reinaromerojorge@gmail.com>
2023-01-18 20:57:11 +0100

    Correpción de errores

--- parkingmanagerDemo/src/main/java/com/rociojorge/parkingmanagerDemo/user/UserController.java
+++ parkingmanagerDemo/src/main/java/com/rociojorge/parkingmanagerDemo/user/UserController.java
@@ -1,6 +1,7 @@
 package com.rociojorge.parkingmanagerDemo.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class UserController {
@@ -8,7 +9,9 @@ public class UserController {
 
     private UserRepository repository = new UserRepository();
         
-    public String displayUsers() {
+    public String displayUsers(Model model) {
+        model.addAttribute("users", repository.getAll());
+        return "userlist";
         
     }
 }
--- parkingmanagerDemo/src/main/java/com/rociojorge/parkingmanagerDemo/user/UserRepository.java
+++ parkingmanagerDemo/src/main/java/com/rociojorge/parkingmanagerDemo/user/UserRepository.java
@@ -1,15 +1,16 @@
 package com.rociojorge.parkingmanagerDemo.user;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
 
-
+    
     public List<User> getAll(){
 
         //TODO : Eliminar este codigo
         ArrayList<User> users = new ArrayList<>();
-        users.add(new User(nombre: "rocio", email: "prueba@gmail.com"));
+        users.add(new User("rocio", "prueba@gmail.com"));
         return users;
 
     }
